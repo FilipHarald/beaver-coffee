53517f2caf0290b17439676d72158abf58d53bb9
kallebornemark <kalle.bornemark@gmail.com>
2017-05-22 14:42:46 +0200

    Added ManageCustomers to routes

--- vue/router.js
+++ vue/router.js
@@ -3,14 +3,16 @@ import Home from './components/Home'
 import AddOrder from './components/AddOrder'
 import AddCustomer from './components/AddCustomer'
 import ManageStock from './components/ManageStock'
+import ManageCustomers from './components/ManageCustomers'
 import ManageEmployees from './components/ManageEmployees'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/add_order', component: AddOrder },
+  { path: '/add_customer', component: AddCustomer },
   { path: '/manage_stock', component: ManageStock },
-  { path: '/manage_employees', component: ManageEmployees },
-  { path: '/add_customer', component: AddCustomer }
+  { path: '/manage_customers', component: ManageCustomers },
+  { path: '/manage_employees', component: ManageEmployees }
 ]
 
 export default new VueRouter({
