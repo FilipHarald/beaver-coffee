962062deb38c852f797951af121b7c0d247ca3cf
kallebornemark <kalle.bornemark@gmail.com>
2017-05-22 13:01:41 +0200

    Added delete button to manage stock

--- vue/components/ManageStock.vue
+++ vue/components/ManageStock.vue
@@ -5,11 +5,11 @@
     </div>
     <div class="card-content">
       <div class="field">
-        <label for="" class="label">Select a product</label>
+        <label for="" class="label">Select an item</label>
         <div class="control">
           <span class="select is-fullwidth">
             <select v-model="selectedProduct">
-              <option v-for="product in products" :key="product._id" :value="product">{{ product.name }}</option>
+              <option v-for="item in items" :key="item._id" :value="item">{{ item.name }}</option>
             </select>
           </span>
         </div>
@@ -18,14 +18,14 @@
       <div class="columns">
         <div class="column">
           <div class="field">
-            <label for="" class="label">ID</label>
-            <input type="text" class="input control" v-model="selectedProduct._id">
+            <label for="" class="label">Name</label>
+            <input type="text" class="input control" v-model="selectedProduct.name">
           </div>
         </div>
         <div class="column">
           <div class="field">
-            <label for="" class="label">Name</label>
-            <input type="text" class="input control" v-model="selectedProduct.name">
+            <label for="" class="label">ID</label>
+            <input type="text" class="input control" v-model="selectedProduct._id">
           </div>
         </div>
       </div>
@@ -37,14 +37,19 @@
             <input type="text" class="input control" v-model="selectedProduct.unit">
           </div>
         </div>
-        <div class="column"></div>
+        <div class="column">
+          <div class="field">
+            <label for="" class="label">Amount</label>
+            <input type="number" class="input control" v-model="selectedProduct.amount">
+          </div>
+        </div>
       </div>
 
       <hr>
 
       <div class="columns">
         <div class="column">
-          <button class="button is-danger is-fullwidth">Delete product</button>
+          <button class="button is-danger is-fullwidth" @click="del">Delete item</button>
         </div>
         <div class="column">
           <button class="button is-success is-fullwidth" @click="save">Save</button>
@@ -64,27 +69,37 @@
     data() {
       return {
         selectedProduct: {},
-        products: []
       }
     },
 
-    created() {
-      this.getProducts()
+    computed: {
+      items() {
+        if (this.store != null)
+          return this.store.stock
+        return []
+      }
     },
 
     methods: {
-      getProducts() {
-        fetch('/api/products', { method: 'get' })
-        .then(result => result.json())
-        .then(productsJson => this.products = productsJson.filter(x => x.productType === 'Ingredient'))
-        .catch(err => console.log(err))
-      },
-
-      save() {
+       save() {
         fetch(`/api/stores/${this.store._id}/products/${this.selectedProduct._id}`, {
           method: 'post',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(this.selectedProduct)
+        })
+        .then(result => {
+          if (!result.ok) {
+            result.json().then(x => {
+              this.errors = result.status < 500 ? x.message : x.errmsg
+            })
+          } else { this.reset() }
+        })
+        .catch(err => console.log(err))
+      },
+
+      del() {
+        fetch(`/api/stores/${this.store._id}/products/${this.selectedProduct._id}`, {
+          method: 'delete',
         })
         .then(result => {
           if (!result.ok) {
