fb0d50053e4b85ae1994515ff11e8ab452bdc426
kallebornemark <kalle.bornemark@gmail.com>
2017-05-23 10:05:25 +0200

    Filled stock in stores

--- src/db/seeders/stores.js
+++ src/db/seeders/stores.js
@@ -14,145 +14,194 @@ const seed = async (mongoose) => {
   }
 }
 
+const fullStock = [
+  {
+    name: '2% Milk',
+    productType: 'Ingredient',
+    unit: 'liter',
+    amount: 50,
+  },
+  {
+    name: 'Espresso Roast',
+    unit: 'kg',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Whole Bean French Roast',
+    unit: 'kg',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Whole Bean Light Roast',
+    unit: 'kg',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Espresso Roast',
+    unit: 'kg',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Cocoa Mix',
+    unit: 'kg',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Skim Milk',
+    unit: 'liter',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Soy Milk',
+    unit: 'liter',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Whole Milk',
+    unit: 'liter',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: '2% Milk',
+    unit: 'liter',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Cream',
+    unit: 'kg',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Vanilla Syrup',
+    unit: 'liter',
+    productType: 'Ingredient',
+    amount: 50,
+  },
+  {
+    name: 'Irish Cream Syrup',
+    unit: 'liter',
+    productType: 'Ingredient',
+    amount: 50,
+  }
+]
+
 const stores = [
+{
+  name: 'BeaverCoffee Malmö',
+  location: {
+    address: 'Lolvägen 1',
+    zip: '1337',
+    country: {
+      name: 'Sweden',
+      currency: 'SEK',
+      countryCode: 'SE'
+    }
+  },
+  stock: fullStock,
+  employees: [
   {
-    name: 'BeaverCoffee Malmö',
+    personId: '19861212-3939',
+    name: 'Albert Einstein',
     location: {
-      address: 'Lolvägen 1',
-      zip: '1337',
+      address: 'Alalalal',
+      zip: '1234',
       country: {
-        name: 'Sweden',
-        currency: 'SEK',
-        countryCode: 'SE'
+        name: 'Germany'
       }
     },
-    stock: [
-      {
-        name: '2% Milk',
-        productType: 'Ingredient',
-        unit: 'liter',
-        amount: 50
-      },
-      {
-        name: 'Espresso Roast',
-        productType: 'Ingredient',
-        unit: 'kg',
-        amount: 50
-      }
-    ],
-    employees: [
-      {
-        personId: '19861212-3939',
-        name: 'Albert Einstein',
-        location: {
-          address: 'Alalalal',
-          zip: '1234',
-          country: {
-            name: 'Germany'
-          }
-        },
-        position: {
-          name: 'Cashier',
-          workPercentage: 100
-        }
-      },
-      {
-        personId: '19991212-3939',
-        name: 'Bruce Wayne',
-        location: {
-          address: 'Waynes Mannor',
-          zip: '222333',
-          country: {
-            name: 'USA'
-          }
-        },
-        position: {
-          name: 'Manager',
-          workPercentage: 100
-        }
-      },
-      {
-        personId: '1337-7331',
-        name: 'Winston Churchill',
-        location: {
-          address: 'Nottingham street 4',
-          zip: '9001',
-          country: {
-            name: 'United Kingdom'
-          }
-        },
-        position: {
-          name: 'Cashier',
-          workPercentage: 50
-        },
-        comments: []
-      }
-    ]
+    position: {
+      name: 'Cashier',
+      workPercentage: 100
+    }
   },
   {
-    name: 'BeaverCoffee Lund',
+    personId: '19991212-3939',
+    name: 'Bruce Wayne',
     location: {
-      address: 'Lolvägen 1',
-      zip: '1337',
+      address: 'Waynes Mannor',
+      zip: '222333',
       country: {
-        name: 'Sweden',
-        currency: 'SEK',
-        countryCode: 'SE'
+        name: 'USA'
       }
     },
-    stock: [
-      {
-        name: '2% Milk',
-        productType: 'Ingredient',
-        unit: 'liter',
-        amount: 10
-      },
-      {
-        name: 'Espresso Roast',
-        productType: 'Ingredient',
-        unit: 'kg',
-        amount: 60
-      },
-      {
-        name: 'Cocoa Mix',
-        productType: 'Ingredient',
-        unit: 'kg',
-        amount: 100
-      }
-    ],
-    employees: [
-      {
-        personId: '19861212-393',
-        name: 'Kalle Anka',
-        location: {
-          address: 'Alalalal',
-          zip: '1234',
-          country: {
-            name: 'Germany'
-          }
-        },
-        position: {
-          name: 'Cashier',
-          workPercentage: 100
-        }
-      },
-      {
-        personId: '19991212-393',
-        name: 'Steve Rogers',
-        location: {
-          address: 'Waynes Mannor',
-          zip: '222333',
-          country: {
-            name: 'USA'
-          }
-        },
-        position: {
-          name: 'Manager',
-          workPercentage: 100
-        }
-      }
-    ]
+    position: {
+      name: 'Manager',
+      workPercentage: 100
+    }
   },
+  {
+    personId: '1337-7331',
+    name: 'Winston Churchill',
+    location: {
+      address: 'Nottingham street 4',
+      zip: '9001',
+      country: {
+        name: 'United Kingdom'
+      }
+    },
+    position: {
+      name: 'Cashier',
+      workPercentage: 50
+    },
+    comments: []
+  }
+  ]
+},
+{
+  name: 'BeaverCoffee Lund',
+  location: {
+    address: 'Lolvägen 1',
+    zip: '1337',
+    country: {
+      name: 'Sweden',
+      currency: 'SEK',
+      countryCode: 'SE'
+    }
+  },
+  stock: fullStock,
+  employees: [
+  {
+    personId: '19861212-393',
+    name: 'Kalle Anka',
+    location: {
+      address: 'Alalalal',
+      zip: '1234',
+      country: {
+        name: 'Germany'
+      }
+    },
+    position: {
+      name: 'Cashier',
+      workPercentage: 100
+    }
+  },
+  {
+    personId: '19991212-393',
+    name: 'Steve Rogers',
+    location: {
+      address: 'Waynes Mannor',
+      zip: '222333',
+      country: {
+        name: 'USA'
+      }
+    },
+    position: {
+      name: 'Manager',
+      workPercentage: 100
+    }
+  }
+  ]
+},
 ]
 
 export default {
